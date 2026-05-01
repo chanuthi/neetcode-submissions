85ea7038f36ee8fea985676e38de37d4c7e27302
chanuthi <36820617+chanuthi@users.noreply.github.com>
2026-05-01 14:53:36 -0500

    Add: python-multiple-except-blocks - submission-1

---
+++ Python For Beginners/python-multiple-except-blocks/submission-1.py
@@ -0,0 +1,21 @@
+def divide_numbers(a: str, b: str) -> None:
+    pass    
+    try:
+        num1 = int(a)
+        num2 = int(b)
+        value = num1 / num2
+        print(value)
+    except ZeroDivisionError:
+        print("Error: Division by zero!")
+    except ValueError:
+        print("Error: Invalid value!")
+    
+
+
+    
+
+
+# do not modify below this line
+divide_numbers("10", "2")
+divide_numbers("12", "0")
+divide_numbers("2", "not a number")
